cf31ae708fedee2d32186f4f2f43c8a13cf43282
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2024-09-01 14:23:42 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './LoginPage';  // Ensure the correct file path
-import RegistorPage from './RegistorPage';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import RegistorPage from "./RegistorPage";
 
 const App = () => {
   return (
-    <div>
+    <div
+      style={{
+        backgroundColor: "#2C2C2C",  
+        color: "white",             
+        minHeight: "100vh",         
+        padding: "20px"             
+      }}
+    >
+      <h1
+        style={{
+          fontFamily: "'Playfair Display', serif",
+          fontWeight: "bold",
+          textAlign: "center",
+          marginTop: "20px",
+        }}
+      >
+        Welcome to User Authentication System
+      </h1>
+
       <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<LoginPage />} />
-          <Route path = '/registor' element={<RegistorPage/>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/registor" element={<RegistorPage />} />
         </Routes>
       </BrowserRouter>
     </div>
