0d04031d41afdd1843190acfbdd53d5cf81f2d5f
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2024-09-14 01:21:53 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,18 +7,27 @@ const App = () => {
   return (
     <div
       style={{
-        backgroundColor: "#1B1A55",  
-        color: "white",             
-        minHeight: "100vh",         
-        padding: "20px"             
+        backgroundColor: "#282c34",  
+        color: "#f5f5f5",           
+        minHeight: "100vh",
+        padding: "20px",
+        fontFamily: "'Playfair Display', serif",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
       }}
     >
       <h1
         style={{
-          fontFamily: "'Playfair Display', serif",
           fontWeight: "bold",
-          textAlign: "center",
-          marginTop: "20px",
+          marginBottom: "20px",
+          fontSize: "2.5rem",
+          background: "linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,0,150,1) 100%)",
+          WebkitBackgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+          padding: "10px",
         }}
       >
         Welcome to User Authentication System 🔒
